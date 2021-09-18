cb1b89947905a0b6a67eeb2c370f2a6d4d3fbd66
henryorsborn <henryorsborn@gmail.com>
2021-09-18 00:34:12 -0400

    Added player Attrs

--- core/entities/player.py
+++ core/entities/player.py
@@ -9,4 +9,13 @@ class Player(Entity):
         :param start_index: list[int, int]
         """
         super().__init__(start_index)
-        self.health = None  # todo
+        self.level = 1
+        self.hp = 300
+        self.mp = 100
+        self.str = 5
+        self.def_ = 5
+        self.mag = 5
+        self.mgdf = 5
+        self.spd = 5
+        self.luck = 5
+        self.spl = []
