42847d89cbcee5c6fe08801cd28f6c30d9c9858d
henryorsborn <henryorsborn@gmail.com>
2021-09-19 23:35:24 -0400

    refractor of enemy serialization

--- core/battle/enemy.py
+++ core/battle/enemy.py
@@ -1,28 +1,28 @@
 from core.battle.attack import Attack
 from core.battle.attack_effect import AttackEffect
-from yaml import load
+from yaml import safe_load
 
 class Enemy(object):
 
-    def __init__(self, type_: str, class_: str, name: str, path_to_sprite: str, max_hp: int, hp: int,
-                 max_mp: int, mp: int, strength: int, defense: int, magic: int,
-                 magic_defense: int, speed: int, resistance: list, elem_resistance: list,
-                 elem_absorb: list, elem_weak: list, attacks: list, script: dict):
+    def __init__(self, type_: str, class_: str, name: str, path_to_sprite: str, hp: int,
+                 mp: int, str_: int, def_: int, mag: int, mgdf: int, spd: int,
+                 resist: list, elem_resist: list, elem_absorb: list, elem_weak: list,
+                 attacks: list, script: dict):
         self.type_ = type_
         self.class_ = class_
         self.name = name
         self.path_to_sprite = path_to_sprite
-        self.max_hp = max_hp
+        self.max_hp = hp
         self.hp = hp
-        self.max_mp = max_mp
+        self.max_mp = mp
         self.mp = mp
-        self.strength = strength
-        self.defense = defense
-        self.magic = magic
-        self.magic_defense = magic_defense
-        self.speed = speed
-        self.resistance = resistance if resistance is not [] else None
-        self.elem_resistance = elem_resistance if elem_resistance is not [] else None
+        self.strength = str_
+        self.defense = def_
+        self.magic = mag
+        self.magic_defense = mgdf
+        self.speed = spd
+        self.resistance = resist if resist is not [] else None
+        self.elem_resistance = elem_resist if elem_resist is not [] else None
         self.elem_absorb = elem_absorb if elem_absorb is not [] else None
         self.elem_weak = elem_weak if elem_weak is not [] else None
         self.attacks = attacks
@@ -31,7 +31,7 @@ class Enemy(object):
     @staticmethod
     def read_and_deserialize_yml(path: str = ""):
         with open(path) as file:
-            content = load(file.read())["monster"]
+            content = safe_load(file.read())["monster"]
 
         attacks = []
         for attack in content["attacks"]:
@@ -43,24 +43,11 @@ class Enemy(object):
                     effects.append(AttackEffect(**effect))
                 attack["eff"] = effects
             attacks.append(Attack(**attack))
+        content["attacks"] = attacks
+        content["resist"] = content["resist"].split()
+        content["elem_resist"] = content["elem_resist"].split()
+        content["elem_absorb"] = content["elem_absorb"].split()
+        content["elem_weak"] = content["elem_weak"].split()
 
-        return Enemy(content["type"],
-                     content["class"],
-                     content["name"],
-                     content["path_to_sprite"],
-                     content["stats"]["hp"],
-                     content["stats"]["hp"],
-                     content["stats"]["mp"],
-                     content["stats"]["mp"],
-                     content["stats"]["str"],
-                     content["stats"]["def"],
-                     content["stats"]["mag"],
-                     content["stats"]["mgdf"],
-                     content["stats"]["spd"],
-                     content["stats"]["resist"].split(),
-                     content["stats"]["elem_resist"].split(),
-                     content["stats"]["elem_absorb"].split(),
-                     content["stats"]["elem_weak"].split(),
-                     attacks,
-                     content["script"])
+        return Enemy(**content)
 
